1c12885c73edf86e432ad5c62f060558a22610c5
Kyryl2 <krasiuk.4884@gmail.com>
2024-04-01 22:32:15 +0200

    ок

--- js/task-3.js
+++ js/task-3.js
@@ -10,7 +10,7 @@ const userName = document.querySelector("input#name-input");
 const newName = document.querySelector("span#name-output");
 userName.addEventListener("input", changeName);
 function changeName() {
-  if (userName.value.trim() !== "") {
+  if (userName.value.trim()) {
     newName.textContent = userName.value.trim();
   } else {
     newName.textContent = "Anonymous";
